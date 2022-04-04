2c3d639ad0e433bd69cea546351e79cb7d229531
DVDFE <davidfe.pro@gmail.com>
2022-04-04 15:02:17 +0200

    commit

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
-		'X-RapidAPI-Key': '1e73939f2fmsha4afb2a87da5556p1074d1jsn2a756cb71d41'
-	}
+   method: 'GET',
+   headers: {
+      'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
+      'X-RapidAPI-Key': '1e73939f2fmsha4afb2a87da5556p1074d1jsn2a756cb71d41'
+   }
 };
+const btn = document.querySelector('#btn').addEventListener('click', function () {
+   fetch(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${sname}&fname=${fname}`, options)
+   .then(response => response.json())
+   .then(response => console.log(response))
+   .catch(err => console.error(err))
+})
 
-fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=david&fname=fifi', options)
-	.then(response => response.json())
-	.then(data => {
-        const snamei = addsnamei(data.sname)
-        const fnamei = addFnamei(data.fname)
-        const h2 = addH2(data.percentage)
-        const h3 = addAresult(data.result)
-        console.log(data)        
-    });
-    
-    function addsnamei(){
-       const snamei = document.getElementById('sname').value
-       return snamei
-    }
+const fname = document.querySelector('#fname').addEventListener('input', function() {
+   return fname
+})
 
-    function addFnamei(){
-        const fnamei = document.getElementById('fname').value
-        return fnamei
-     }
- 
-
-     function addH2(){
-        const percentage = document.getElementById('percentage')
-        return percentage
-     }
- 
-    
-     function addAresult(){
-        const result = document.getElementById('result')
-        return result
-     }
- 
-
-
+const sname = document.querySelector('#sname').addEventListener('input', function() {
+   return sname
+})
 
